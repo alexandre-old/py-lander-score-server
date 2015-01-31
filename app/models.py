9af2dc0daf18780e780e766ddf0a7cd1c0de5f9a
Alexandre Souza <alexandre_@fastmail.com>
2015-01-30 23:34:50 -0200

    added models

---
+++ app/models.py
@@ -0,0 +1,34 @@
+from . import db
+
+
+class Player(db.Model):
+
+    """Docstring for Player. """
+
+    id = db.Column(db.Integer, primary_key=True)
+    nick = db.Column(db.String(255))
+    fuel = db.Column(db.Float)
+    multiplier = db.Column(db.Float)
+    score = db.Column(db.Float)
+
+    def __init__(self, nick, fuel, multiplier, score):
+        """TODO: Docstring for __init__.
+
+        :nick: TODO
+        :fuel: TODO
+        :multiplier: TODO
+        :score: TODO
+        :returns: TODO
+
+        """
+        self.nick = nick
+        self.fuel = fuel
+        self.multiplier = multiplier
+        self.score = score
+
+    def __repr__(self):
+        """TODO: Docstring for __repr__.
+        :returns: TODO
+
+        """
+        return '<Player %r>' % self.nick

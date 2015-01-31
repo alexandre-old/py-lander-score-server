ee74ccc54e388e30d5884deb8ded58705a323a31
Alexandre Souza <alexandre_@fastmail.com>
2015-01-31 00:48:06 -0200

    updated db name

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ import os
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-DB_PATH = os.path.join(BASE_DIR, 'pylander')
+DB_PATH = os.path.join(BASE_DIR, 'pylander.db')
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{0}'.format(DB_PATH)
